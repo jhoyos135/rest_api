4d9d47291b647291281bf561bbccbea40793666b
Julian Hoyos <jhoyos135@gmail.com>
2018-10-10 09:07:26 -0400

    new changes in the front end

--- public/script.js
+++ public/script.js
@@ -1,27 +1,27 @@
 
 let submit = document.querySelector('.submit');
+var form = document.getElementsByName('contact-form')[0];
+var isLoading = false;
 
 submit.onclick = function(e) {
     e.preventDefault();
-
-    if(submit.done) {return;}
-
     let lng = document.querySelector("input.lng");
     let lat = document.querySelector("input.lat");
     let url = `/api/customers?lng=${lng.value}&lat=${lat.value}`;
 
     fetch(url).then( (data) => {
 
-       
         return data.json();
 
-    }).then(function(myCustomers) {
-        let obj = myCustomers;
-
-    for(let i = 0; i < obj.length; i++) {
+    }).then(( myCustomers) => {
 
 
-        let customers_found = `
+
+    let obj = myCustomers;
+
+        for(let i = 0; i < obj.length; i++) {
+
+     let customers_found = `
         
     <li>
         <span>Name: ${obj[i].name}</span>
@@ -30,15 +30,14 @@ submit.onclick = function(e) {
 
         `;
         document.querySelector("#customers").innerHTML += customers_found;
-
         
-}   
-    
+            }
+          
     });
 
-    submit.done = true;
-    
+    form.reset();
 };
+
 
 
 
